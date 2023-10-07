59954f80faa542355e0444b2b83df67eea61180b
shamim-5 <shamim.somadder5@gmail.com>
2023-10-07 14:18:52 +0600

    initalized a nextjs project with antd and tailwindcss and remove unnecessary things

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+
+export default function Home() {
+  return (
+    <main className="text-red-900">
+      <p>check tailwindcss</p>
+      <Button type="primary" ghost className='my-12'>Button</Button>
+    </main>
+  );
+}
